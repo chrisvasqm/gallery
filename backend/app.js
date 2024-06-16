287b91e4c6503c4fb7f65907c957c5172c99719f
chrisvasqm <christianvasquezmelo@gmail.com>
2024-06-16 00:34:21 -0400

    Render multiple images

--- backend/app.js
+++ backend/app.js
@@ -17,5 +17,20 @@ app.use('/api/image/:file', (req, res) => {
   res.send(getSignedUrl(file));
 });
 
+app.use('/api/images', (_, res) => {
+  const images = [
+    'wallpaper-1.jpg',
+    'wallpaper-2.jpg',
+    'wallpaper-3.jpg',
+    'wallpaper-4.jpg',
+    'wallpaper-5.jpg',
+    'wallpaper-6.jpg',
+  ];
+
+  const imageUrls = images.map(image => getSignedUrl(image));
+
+  res.send(imageUrls);
+})
+
 const port = process.env.PORT = 3001;
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,18 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [image, setImage] = useState('');
+  const [images, setImages] = useState([]);
 
   const handleImageLoad = async () => {
-    const response = await axios.get('http://localhost:3001/api/image/wallpaper-1-min.webp');
-    setImage(response.data);
+    const response = await axios.get('http://localhost:3001/api/images');
+    setImages(response.data);
   }
 
   return (
     <>
       <h1>Gallery</h1>
       <button onClick={handleImageLoad}>Load image</button>
-      {image && <img src={image} width={300} style={{ display: 'block' }} />}
+      {images && images.map(image => <img src={image} width={300} style={{ display: 'block' }} />)}
     </>
   )
 }
